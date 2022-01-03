fb8d63f321ea157c0f853d4aca1e0817ee11ef03
Milan <milan.3sa@gmail.com>
2022-01-02 20:45:41 -0500

    Form Addition

--- main.js
+++ main.js
@@ -1,43 +1,69 @@
-const navMenu = document.getElementById('nav-menu'),
-    toggleMenu = document.getElementById('nav-toggle'),
-    closeMenu = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  toggleMenu = document.getElementById("nav-toggle"),
+  closeMenu = document.getElementById("nav-close");
 
+toggleMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
 
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show')
-})
+closeMenu.addEventListener("click", () => {
+  navMenu.classList.remove("show");
+});
 
+const navLink = document.querySelectorAll(".nav_link");
 
-closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show')
-})
-
-
-const navLink = document.querySelectorAll('.nav_link')
-
-function linkAction(){
-    navMenu.classList.remove('show')
+function linkAction() {
+  navMenu.classList.remove("show");
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
+const sections = document.querySelectorAll("section[id]");
 
-const sections = document.querySelectorAll('section[id]')
+window.addEventListener("scroll", scrollActive);
 
-window.addEventListener('scroll', scrollActive)
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-function scrollActive(){
-    const scrollY= window.pageYOffset
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute('id')
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
+}
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav_menu a[href*='+ sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('.nav_menu a[href*='+ sectionId + ']').classList.remove('active')
-        }
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      status.innerHTML = "Thanks for your submission!";
+      form.reset();
+      //   document.getElementById("name").value() = "";
+      document.getElementById("name").reset();
     })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+  form.reset();
 }
+form.addEventListener("submit", handleSubmit);
